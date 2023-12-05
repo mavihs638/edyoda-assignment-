da8faf218da8f593fa93b2f091a624c63f73a25c
Muskan Gupta <muskan@Muskans-MacBook-Air.local>
2023-12-05 21:54:50 +0530

    first push

---
+++ linear data structure/StringRotation.java
@@ -0,0 +1,44 @@
+
+
+class StringRotation {
+	
+	static boolean areRotations(String s1, String s2)
+	{
+		if (s1.length() != s2.length()) {
+			return false;
+		}
+		else {
+			for (int i = 0; i < s1.length(); i++) {
+				
+				if (s1.charAt(i) == s2.charAt(0)) {
+					
+					if (s1.substring(i).equals(s2.substring(
+							0, s1.length() - i))) {
+						
+						if (s1.substring(0, i).equals(
+								s2.substring(s1.length()
+											- i)))
+							return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+
+
+	public static void main(String[] args)
+	{
+		String str1 = "AACD";
+		String str2 = "ACDA";
+
+		// Fuinction call
+		if (areRotations(str1, str2))
+			System.out.println(
+				"Strings are rotations of each other");
+		else
+			System.out.printf(
+				"Strings are not rotations of each other");
+	}
+}
+
